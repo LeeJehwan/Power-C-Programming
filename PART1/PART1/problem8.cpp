27f3e7055a6660011b4abf3a0b094d2d768e1b8f
LeeJehwan <wpghks7@naver.com>
2019-05-07 01:42:09 +0900

    add p8

--- PART1/PART1/problem8.cpp
+++ PART1/PART1/problem8.cpp
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
-int main() {
+void dec_to_bin(int num) {
+	if (num == 0)
+		return;
 
+	dec_to_bin(num / 2);
+	printf("%d", num % 2);
+}
+
+
+int main() {
+	int num;
+	printf("10진수 정수 입력: ");
+	scanf("%d", &num);
+
+	printf("%d를 2진수로 변환하면: ", num);
+	dec_to_bin(num);
+	printf("\n");
 
 	return 0;
 }
