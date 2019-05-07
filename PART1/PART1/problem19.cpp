024417d117a26ab17d54c412a1bf325ad76b236a
LeeJehwan <wpghks7@naver.com>
2019-05-07 16:18:34 +0900

    add p19

--- PART1/PART1/problem19.cpp
+++ PART1/PART1/problem19.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 #include <stdio.h>
 using namespace std;
+#define FACT_ERROR -1
 
 int factorial(int n) {
+	if (n < 1)
+		return FACT_ERROR;
 	if (n == 1)
 		return n;
-	
 	return n * factorial(n - 1);
 }
 
 int main() {
 	int n;
-	while (1) {
-		cout << "숫자 입력(1이상): ";
-		cin >> n;
-		if (n > 0)
-			break;
-		cout << "1 이상의 숫자를 입력해주세요" << endl;
-	}
+	cout << "숫자 입력(1이상): ";
+	cin >> n;
 
-	cout << n << "! = " << factorial(n) << endl;
+
+	int ret = factorial(n);
+	if (ret == FACT_ERROR)
+		cout << "입력 오류" << endl;
+	else
+		cout << n << "! = " << ret << endl;
 	
 	return 0;
 }
