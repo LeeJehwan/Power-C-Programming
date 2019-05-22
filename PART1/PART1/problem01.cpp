54bb0a6a88bd7364f85a785c05ffe96f42cd49b0
LeeJehwan <wpghks7@naver.com>
2019-05-22 22:21:24 +0900

    add test_file

--- PART1/PART1/problem01.cpp
+++ PART1/PART1/problem01.cpp
@@ -9,8 +9,7 @@ int main() {
 	char str[100];
 
 	printf("[문자, 정수, 실수, 문자열]의 포맷으로 입력: ");
-	scanf("%c, %d, %lf, %s", &c, &num1, &num2, str);
+	scanf("%c, %d\n %lf, %s", &c, &num1, &num2, str);
 	printf("입력된 데이터 출력: %c, %d, %lf, %s \n", c, num1, num2, str);
-
 	return 0;
 }
